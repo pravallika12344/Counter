c5aae89cc2cc39a1af49e4aa24740ea6db14f050
pravallika <pathipatipravallika@gmail.com>
2023-09-28 21:55:17 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import './App.css';
+import styles from "./app.module.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [number, setNumber] = useState(1)
+
+  const inc = () => {
+    setCounter(counter + number);
+  };
+  const inp = (event) => {
+    const num = Number(event.target.value);
+    setNumber(num);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.body}>
+      <div className={styles.App}>
+        <h1 style={{ color: counter < 0 ? "red" : "#63da75" }}>{counter}</h1>
+        <input className={styles.input} type="number" id="number" onChange={inp}></input>
+        <div className={styles.buttons}>
+          <button className={styles.button} onClick={inc}>+</button>
+          <button className={styles.button} onClick={() => setCounter(0)}>reset</button>
+          <button className={styles.button} onClick={() => setCounter(counter - number)}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
